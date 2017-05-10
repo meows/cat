3858e1a1c463cf158ea52e33e46d2e1d121ec18f
saynee <saynee@github>
2017-05-10 12:33:54 -0700

    added VIEW_ALL action type

--- state/types.js
+++ state/types.js
@@ -2,8 +2,11 @@ class Types {
    static TODO_ADD     = 'TODO_ADD'
    static TODO_DELETE  = 'TODO_DELETE'
    static TODO_TOGGLE  = 'TODO_TOGGLE'
+
    static VIEW_CURRENT = 'VIEW_CURRENT'
    static VIEW_DONE    = 'VIEW_DONE'
+   static VIEW_ALL     = 'VIEW_ALL'
+
    static ERROR        = 'ERROR'
 }
 
