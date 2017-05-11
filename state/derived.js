f80d47b744fa9ae901fd09ac064a90986f797bf0
meow <meow@iceland>
2017-05-11 08:44:01 -0700

    0

---
+++ state/derived.js
@@ -0,0 +1,13 @@
+import T from './types'
+
+const visibility = function(view_type, state) {
+   switch (view_type) {
+      case T.VIEW_ALL     : return state
+      case T.VIEW_CURRENT : return state.filter(todo => !todo.done)
+      case T.VIEW_DONE    : return state.filter(todo => todo.done)
+   }
+}
+
+export {
+   visibility,
+}
