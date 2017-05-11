202c5d41c86446fdba3148a6c73d9e29e0b286cb
meow <meow@iceland>
2017-05-11 08:35:18 -0700

    0

--- components/footer.js
+++ components/footer.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import T from '../state/types'
+import A from '../state/actions'
 
-function Footer({ fn1, fn2 }) {
+function Footer() {
+   const all     = A.goSee(T.VIEW_ALL)
+   const current = A.goSee(T.VIEW_CURRENT)
+   const done    = A.goSee(T.VIEW_DONE)
+
    return (
       <footer>
+
       </footer>
    )
 }
