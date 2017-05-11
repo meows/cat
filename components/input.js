b5f2d1d493d3b64a1c178ab4c53116aaa6a059d4
meow <meow@iceland>
2017-05-11 09:57:12 -0700

    0

--- components/input.js
+++ components/input.js
@@ -1,13 +1,32 @@
 import React from 'react'
 
 // Input :: Fn -> JSX
-function Input({ onClick }) {
+function $Input({ onClick }) {
    return (
       <section id='Input'>
          <input name='task' />
          <button onClick={onClick} type='button'>Add Task</button>
       </section>
    )
+}
+
+class Input extends React.Component {
+   state = ''
+
+   constructor(props) { super(props) }
+
+   render() {
+      return <input
+         value={this.state}
+         onChange={this.update}
+      />
+   }
+
+   componentWillUnmount() { this.state = '' }
+
+   update(event) {
+      this.setState(event.target.value)
+   }
 }
 
 export default Input
