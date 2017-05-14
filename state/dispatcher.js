c0043c3b1e9657e1ee97b0e4292767284ca883dc
joe <joe@oak>
2017-05-14 15:36:56 -0700

    style

--- state/dispatcher.js
+++ state/dispatcher.js
@@ -1,5 +1,8 @@
 import store from './store'
 
+// -----------------------------------------------------------------------------
+// Dispatch
+
 const dispatch = (action) => store.dispatch(action)
 
 export default dispatch
