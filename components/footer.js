63c616d4c47efd73eef4dd027b5d3ca4c9303c8f
saynee <saynee@github>
2017-05-13 18:01:38 -0700

    fix to footer

--- components/footer.js
+++ components/footer.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import T from '../state/types'
-import A from '../state/actions'
+import T     from '../state/types'
+import A     from '../state/actions'
 
 function Footer() {
-   const all     = () => A.goSee(T.VIEW_ALL)
-   const current = () => A.goSee(T.VIEW_CURRENT)
-   const done    = () => A.goSee(T.VIEW_DONE)
+   const all     = () => dispatch(A.goSee(T.VIEW_ALL))
+   const current = () => dispatch(A.goSee(T.VIEW_CURRENT))
+   const done    = () => dispatch(A.goSee(T.VIEW_DONE))
 
    return (
       <footer>
