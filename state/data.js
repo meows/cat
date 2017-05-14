6d449512975f955a38af973310bf8668b03e69e6
meow <meow@iceland>
2017-05-14 01:34:48 -0700

    0

--- state/data.js
+++ state/data.js
@@ -1,19 +1,19 @@
 // data :: Todo[]
 const DATA = [
    {
-      task: 'meow',
-      id: 230193013,
-      done: false,
+      task : 'meow',
+      id   : 230193013,
+      done : false,
    },
    {
-      task: 'mix',
-      id: 213099,
-      done: false,
+      task : 'mix',
+      id   : 213099,
+      done : false,
    },
    {
-      task: 'moo',
-      id: 1314,
-      done: true,
+      task : 'moo',
+      id   : 1314,
+      done : true,
    },
 ]
 
