da1ef5e567236492e9ace718edaffb6bf3b0ebff
meow <meow@iceland>
2017-05-16 09:03:51 -0700

    update

--- pages/index.js
+++ pages/index.js
@@ -2,25 +2,19 @@ import React    from 'react'
 import Form     from '../components/form'
 
 import store    from '../state/store'
-import A        from '../state/actions'
-import T        from '../state/types'
-import dispatch from '../state/dispatcher'
 
 /* -----------------------
    Route : /
    Page  : index.js
    ----------------------- */
 
-dispatch(A.addTodo('buy milk'))
-dispatch(A.addTodo('do homework'))
-dispatch({ type: T.TODO_ADD, data: { task: 'feed dog', id: 13131322, done: true }})
-dispatch({ type: T.TODO_ADD, data: { task: 'buy book', id: 11111111, done: true }})
-
 // -----------------------------------------------------------------------------
 // Export
 
-export default (state = store.getState()) => (
-   <div id='App'>
-      <Form todos={state.todos} view={state.view} />
-   </div>
-)
+export default (state = store.getState()) => {
+   return (
+      <div id='App'>
+         <Form todos={state.todos} view={state.view} />
+      </div>
+   )
+}
--- state/derive.js
+++ state/derive.js
@@ -4,12 +4,12 @@ import T from './types'
 // Derived
 
 class Derive {
-   static view(todos = [], view) {
+   static view(todos = [], view = T.VIEW_CURRENT) {
       switch (view) {
          case T.VIEW_ALL     : return todos
          case T.VIEW_CURRENT : return todos.filter(todo => !todo.done)
          case T.VIEW_DONE    : return todos.filter(todo => todo.done)
-         default             : throw new TypeError(`Derive.view() didn't receive expected string.`)
+         default             : new TypeError(`Derive.view() received wrong input: `, todos, view)
       }
    }
 }
