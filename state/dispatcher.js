3554615880bc70ec00e67bdff2c4a53b4a4e2e09
meow <meow@iceland>
2017-05-18 11:09:16 -0700

    update

--- state/dispatcher.js
+++ state/dispatcher.js
@@ -1,8 +1,16 @@
 import store from './store'
+import A     from './actions'
 
 // -----------------------------------------------------------------------------
-// Dispatch
+// Dispatchers
 
 const dispatch = (action) => store.dispatch(action)
+
+class Dispatchers {
+   static todoAdd    = (task) => dispatch(A.addTodo(task))
+   static todoToggle = (id)   => dispatch(A.toggleTodo(id))
+   static todoDelete = (id)   => dispatch(A.toggleTodo(id))
+   static goSee      = (view) => dispatch(A.goSee(view))
+}
 
 export default dispatch
