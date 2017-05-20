a68bcd82f2e2a9c7df8414e9e905bd1194ce2ed8
meow <meow@iceland>
2017-05-19 19:06:37 -0700

    update

--- state/dispatcher.js
+++ state/dispatcher.js
@@ -10,8 +10,8 @@ class Dispatchers {
    static todoAdd    = (task) => dispatch(A.addTodo(task))
    static todoToggle = (id)   => dispatch(A.toggleTodo(id))
    static todoDelete = (id)   => dispatch(A.toggleTodo(id))
-   static goSee      = (view) => dispatch(A.goSee(view))
    static clearDone  = ()     => dispatch(A.clearDone())
+   static goSee      = (view) => dispatch(A.goSee(view))
 }
 
-export default dispatch
+export default Dispatchers
