a2058d60ffe20e4a44209c2049ad4d0727138542
meow <meow@iceland>
2017-05-20 08:27:30 -0700

    update

--- components/Footer.js
+++ components/Footer.js
@@ -4,15 +4,11 @@ import A        from '../state/actions'
 import dispatch from '../state/dispatcher'
 
 function Footer() {
-   const all     = () => dispatch(A.goSee(T.VIEW_ALL))
-   const current = () => dispatch(A.goSee(T.VIEW_CURRENT))
-   const done    = () => dispatch(A.goSee(T.VIEW_DONE))
-
    return (
       <footer>
-         <button type="button" onClick={all}>All</button>
-         <button type="button" onClick={current}>Current</button>
-         <button type="button" onClick={done}>Done</button>
+         <button type="button" onClick={dispatch.goSee(T.VIEW_ALL)}>All</button>
+         <button type="button" onClick={dispatch.goSee(T.VIEW_CURRENT)}>Current</button>
+         <button type="button" onClick={dispatch.goSee(T.VIEW_DONE)}>Done</button>
       </footer>
    )
 }
